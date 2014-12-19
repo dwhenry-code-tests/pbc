ad8a378789d16d19ea9ae3dfaff3f17f56ec9f11
David Henry <dw_henry@yahoo.com.au>
2014-12-19 21:18:46 +0000

    Move to spec for testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  class InternalConstraint
+    def self.matches?(request)
+      # TODO: have a secure access token..
+      # and possible IP address filtering using a config file to specify valid IP ranges
+      request.query_parameters['token'] == 'private'
+    end
+  end
+
+  constraints(subdomain: InternalConstraint) do
+    get '/locations/:country_code' => 'private/locations#show'
+    get '/tag_groups/:country_code' => 'private/tag_groups#show'
+    post '/evaluate_target' => 'private/evaluate_target#create'
+  end
+
+  get '/locations/:country_code' => 'public/locations#show'
+  get '/tag_groups/:country_code' => 'public/tag_groups#show'
 
 end
